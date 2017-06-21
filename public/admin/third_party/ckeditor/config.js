43db2edd18ffb1615ceaf9470c732de2ec2f9c28
Anton Axenov <info@webcomfort.ru>
2017-06-21 18:29:27 +0300

    CKeditor - Bootstrap Grid, Code Embed

--- public/admin/third_party/ckeditor/config.js
+++ public/admin/third_party/ckeditor/config.js
@@ -1,41 +1,37 @@
 /**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config )
-{
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-
+CKEDITOR.editorConfig = function( config ) {
     config.skin = 'moono';
-	config.filebrowserBrowseUrl = 		'/public/admin/third_party/elfinder/elfinder.html';
-	config.filebrowserImageBrowseUrl = 	'/public/admin/third_party/elfinder/elfinder.html';
-	config.filebrowserFlashBrowseUrl = 	'/public/admin/third_party/elfinder/elfinder.html';
-	config.filebrowserWindowWidth = '1000';
+    config.filebrowserBrowseUrl = 		'/public/admin/third_party/elfinder/elfinder.html';
+    config.filebrowserImageBrowseUrl = 	'/public/admin/third_party/elfinder/elfinder.html';
+    config.filebrowserFlashBrowseUrl = 	'/public/admin/third_party/elfinder/elfinder.html';
+    config.filebrowserWindowWidth = '1000';
     config.filebrowserWindowHeight = '500';
-	config.defaultLanguage = 'ru';
-	config.contentsCss = '/public/site/css/editorarea.css';
-	config.toolbar = 'WCMS';
-	config.toolbar_WCMS =
-	[
-    ['Maximize','Source'],
-    ['Cut','Copy','Paste','PasteText','PasteFromWord','Templates'],
-    ['Undo','Redo','-','Find','Replace','-','RemoveFormat'],
-    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
-    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-    ['Link','Unlink','Anchor'],
-    ['Image','Flash','MediaEmbed','Table','SpecialChar'],
-    ['Format','Styles']
-	];
+    config.defaultLanguage = 'ru';
+    config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}';
+    config.contentsCss = '/public/site/css/editorarea.css';
+    config.toolbar = 'WCMS';
+    config.toolbar_WCMS =
+        [
+            ['Maximize','Source'],
+            ['Cut','Copy','Paste','PasteText','PasteFromWord','Templates','btgrid'],
+            ['Undo','Redo','-','Find','Replace','-','RemoveFormat'],
+            ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+            ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+            ['Link','Unlink','Anchor'],
+            ['Image','Flash','PasteCode','Table','SpecialChar'],
+            ['Format','Styles']
+        ];
 
-	config.width = '100%';
+    config.width = '100%';
     config.height = '500';
-	config.extraPlugins = 'mediaembed,templates';
-	config.emailProtection = 'encode';
-	config.startupOutlineBlocks = true;
+    config.emailProtection = 'encode';
+    config.extraPlugins = 'btgrid,pastecode';
+    config.startupOutlineBlocks = true;
     config.allowedContent = 'span div p h1 h2 h3 h4 h5 h6 ul ol li strong em blockquote mark del s ins small abbr address footer dl dt code kbd pre var samp i [class,id,title,role]{float}(*); a [href,class,title,name,download,target,id,rel,role]{float}(*); hr table tr th; td [colspan,rowspan,class]{float}(*);img[!src,alt,width,height,class]{float}(*); iframe [*]{*}(*); button [class,type,id,data-toggle]{*}(*);';
 
     config.basicEntities = false;
