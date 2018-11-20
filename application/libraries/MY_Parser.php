47aea3f49aabf483e762f1953f259c677128eec7
Anton Axenov <info@webcomfort.ru>
2018-11-20 15:59:59 +0300

    Parser fix

--- application/libraries/MY_Parser.php
+++ application/libraries/MY_Parser.php
@@ -8,29 +8,29 @@ class MY_Parser extends CI_Parser {
     * @param    string
     * @return	string
     */
-    
-    function parse_modules($template)
-	{
-		preg_match_all('~\{@\s?module\s?(.+?)@\}~', $template, $matches);
-        
-        if(count($matches[1]))
-        {
-            foreach ($matches[1] as $key => $value)
-            {
-                $value = trim($value);
-                $value = preg_replace('!\s+!', ' ', $value);
-                $pieces = explode(" ", $value);
-                $params = array();
-                
-                for ($i = 0, $size = sizeof($pieces); $i < $size; ++$i)
-                {
-                    if($i == 0) $model = $pieces[$i];
-                    if($i > 0) $params[] = $pieces[$i];
-                }
 
-                $template = str_replace($matches[0][$key], module($model,$params), $template);
-            }
-        }
-        return $template;
+	function parse_modules($template)
+	{
+		preg_match_all('~<(div|p)>\{@\s?module\s?(.+?)@\}</(div|p)>~', $template, $matches);
+
+		if(count($matches[2]))
+		{
+			foreach ($matches[2] as $key => $value)
+			{
+				$value = trim($value);
+				$value = preg_replace('!\s+!', ' ', $value);
+				$pieces = explode(" ", $value);
+				$params = array();
+
+				for ($i = 0, $size = sizeof($pieces); $i < $size; ++$i)
+				{
+					if($i == 0) $model = $pieces[$i];
+					if($i > 0) $params[] = $pieces[$i];
+				}
+
+				$template = str_replace($matches[0][$key], module($model,$params), $template);
+			}
+		}
+		return $template;
 	}
 }
