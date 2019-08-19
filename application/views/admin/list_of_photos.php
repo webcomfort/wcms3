13b20b4d66b61f2445da5a082f01b9d117d6db08
Anton Axenov <info@webcomfort.ru>
2019-08-19 18:14:47 +0300

    New list mode for photos

--- application/views/admin/list_of_photos.php
+++ application/views/admin/list_of_photos.php
@@ -25,7 +25,7 @@ foreach ($myEditResult as $value){
 	?>
 	<div class="col-sm-4 col-md-3" id="item-<?=$value['qf1']?>">
 		<div class="thumbnail">
-			<div class="gal-photo-header"><?=$value['qf1']?><?=$value['qf3']?></div>
+			<div class="gal-photo-header"><?=$value['qf3']?></div>
 			<?=$value['file']?>
 			<div class="caption">
 				<p class="text-center"><?=$value['view_button']?> <?=$value['change_button']?> <?=$value['copy_button']?> <?=$value['delete_button']?></p>
